140fbdb5079084e21a8ec0034f9d0ea2c9ae4f64
whkelvin <whkelvin.hsu@gmail.com>
2024-04-03 21:27:40 +0800

    updated env variables

--- src/routes/(site)/tags/[slug]/+page.server.ts
+++ src/routes/(site)/tags/[slug]/+page.server.ts
@@ -1,5 +1,5 @@
 import type { PageData, PageServerLoad } from './$types';
-import { GHOST_URL, GHOST_CONTENT_API_KEY } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import GhostContentAPI, { type PostsOrPages, type Tag } from '@tryghost/content-api';
 
 export interface Post {
@@ -9,8 +9,8 @@ export interface Post {
 
 export const load: PageServerLoad = async ({ params }): Promise<PageData> => {
   const api = new GhostContentAPI({
-    url: GHOST_URL,
-    key: GHOST_CONTENT_API_KEY,
+    url: env.GHOST_URL,
+    key: env.GHOST_CONTENT_API_KEY,
     version: 'v5.0'
   });
 
